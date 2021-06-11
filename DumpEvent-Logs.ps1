7f55a1c396f69b20ae15502a6cde4f7fd7cd6a32
DexterLex98 <suraj98das@gmail.com>
2021-06-11 20:48:17 +0530

    Added Security event logs, Compression for the files into EventLogs.zip

--- DumpEvent-Logs.ps1
+++ DumpEvent-Logs.ps1
@@ -1,14 +1,23 @@
 
-param($savePath)
+param($savePath,$destinationPath)
 
 function EventLogsDump_App($appLogPath){
 Get-Eventlog -LogName application -EntryType Error,Warning | Export-Csv -Path "$($appLogPath)\application_logs.csv"
 }
 
 function EventLogsDump_System($sysLogPath){
-Get-Eventlog -LogName System -EntryType Error,Warning | Export-Clixml -Path "$($sysLogPath)\system_logs.csv"
+Get-Eventlog -LogName System -EntryType Error,Warning,Information | Export-Clixml -Path "$($sysLogPath)\system_logs.csv"
+}
+function EventLogsDump_Security($secLogPath){
+Get-Eventlog -LogName Security -EntryType SuccessAudit,FailureAudit | Export-Csv -Path "$($secLogPath)\Security_logs.csv"
+}
+
+function compress_files($compressPath){
+Compress-Archive -Path "$($compressPath)\*.csv" -DestinationPath "$($destinationPath)\EventLogs.zip"
 }
 
 EventLogsDump_App($savePath)
 EventLogsDump_System($savePath)
+EventLogsDump_Security($savePath)
+compress_files($savePath)
 
